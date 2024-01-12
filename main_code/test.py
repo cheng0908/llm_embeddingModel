f5a60ccba9d5e9b0bbae0f6f6e934f573d93a7db
Ngu Calvin <calvinngu00@gmail.com>
2024-01-12 14:07:12 +0800

    add test.py

---
+++ main_code/test.py
@@ -0,0 +1,20 @@
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+# Only keep post title, headers, and content from the full HTML.
+bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs={"parse_only": bs4_strainer},
+)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+
+docs = loader.load()
+all_splits = text_splitter.split_documents(docs)
+
+print(len(docs[0].page_content))
+print(docs[0].page_content[:500])
